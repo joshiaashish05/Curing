60aaa93864e6f490073b9cf115d6a611afbbbe87
aashish <joshiaashish05@gmail.com>
2024-02-02 22:35:07 +0530

    CustomUserModel Ready to use

--- curingproject/administrator/models.py
+++ curingproject/administrator/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUsers(AbstractUser):
+     UserID = models.AutoField(primary_key=True)
+     def __str__(self):
+        return self.username
